06e29a472b17e36f07b421cdcc7cefd8d32d0228
ArthurORothen <arthurrothen2@gmail.com>
2026-03-24 14:48:00 -0300

    Correção do js,imagens dos albuns e escolha do album inicial

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-let albumAtivo = {
-    imgFundo: document.querySelector('body').style.backgroundImage = "url('imgs/img_X.svg')",
-    imgAlbum: document.querySelector('.imgAlbum'),
-    listaMusicas: [
-        "One",
-        "I'm a Mess",
-        "Sing",
-        "Don't",
-        "Nina",
-        "Photograph"
-    ]
+let imgFundo = document.body.style.backgroundImage = "url('imgs/fundo+.svg')"
+let imgAlbum = document.querySelector('.imgAlbum')
+let listaMusicas = document.querySelector('.listaMusicas')
+
+const listaAlbuns = [
+    {
+        fundo: "imgs/fundo+.svg",
+        capa: "imgs/+EdSheeran.jpg",
+        musicas: ["The A Team", "Drunk", "U.N.I.", "Grade 8", "Wake Me Up", "Small Bump", "This", "The City", "Lego House", "You Need Me, I Don't Need You", "Kiss Me", "Give Me Love"
+        ]
+    },
+
+    {
+        fundo: "imgs/img_X.svg",
+        capa: "imgs/X_cover.png",
+        musicas: ["One", "I'm a Mess", "Sing", "Don't", "Nina", "Photograph", "Bloodstream", "Tenerife Sea", "Runaway", "The Man", "Thinking Out Loud", "Afire Love"]
+    }
+]
+
+function proximo() {
 }
 
-
-
-function proximo(){
-
-}
-
-function anterior(){
+function anterior() {
 
 }
